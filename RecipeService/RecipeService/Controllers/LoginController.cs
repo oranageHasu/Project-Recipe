b6710877afe5f943f2f809c53699e8e5399e834d
OranageHasu <blair.petrachek@gmail.com>
2020-05-03 17:54:48 -0500

    Initial checkin for .net core project

---
+++ RecipeService/RecipeService/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RecipeService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
